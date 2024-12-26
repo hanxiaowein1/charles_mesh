6be266018b3928e75ea607920f0671072fee5498
hanxiaowein1 <hanxiaowein1@github.com>
2024-12-26 23:59:29 +0800

    NEW: make a test of model viewer, not good

--- draw_mesh_test.cpp
+++ draw_mesh_test.cpp
@@ -2,6 +2,8 @@
 #include "basic_type.h"
 
 #include "triangles_shower.h"
+#include "charles_mesh.h"
+#include "mesh_factory.h"
 
 extern std::pair<std::vector<charles_mesh::Point3D>, std::vector<std::vector<int>>> get_tetrahedron();
 
@@ -27,7 +29,46 @@ TEST(GlobalTest, show_mesh_test)
     display_info.b = 0.5f;
 
     std::vector<DisplayInfo<Point3D, std::vector<int>>> mul_display_info{ display_info };
-    show_triangles(mul_display_info);
+    show_triangles_with_model_viewer(mul_display_info);
+}
+
+
+TEST(GlobalTest, draw_small_bunny)
+{
+    std::string small_bunny = "C:\\Users\\hanxi\\Downloads\\simplify_Bunny3.obj";
+    ObjMeshIO obj_mesh_io;
+    auto mesh = obj_mesh_io.load_mesh(small_bunny);
+    DisplayInfo<Point3D, std::vector<int>> display_info;
+    std::unordered_map<decltype(mesh->v_head), int> index_cache;
+    int count = 0;
+    for(const auto& vertex: mesh->vertices)
+    {
+        // file << "v " << vertex->position.x << " " << vertex->position.y << " " << vertex->position.z << std::endl;
+        display_info.vertices.emplace_back(vertex->position);
+        index_cache.emplace(vertex, count);
+        count++;
+    }
+
+    for (const auto& face : mesh->faces)
+    {
+        auto iter = face->half_edge;
+        auto head = iter;
+        std::vector<int> vec_face;
+        do
+        {
+            int index = index_cache.at(iter->vertex);
+            vec_face.emplace_back(index);
+            iter = iter->next;
+        } while (iter != head);
+        display_info.triangles.emplace_back(vec_face);
+        display_info.normals.emplace_back(face->normal());
+    }
+    display_info.r = 0.0f;
+    display_info.g = 1.0f;
+    display_info.b = 0.0f;
+
+    std::vector<DisplayInfo<Point3D, std::vector<int>>> mul_display_info{ display_info };
+    show_triangles_with_model_viewer(mul_display_info);
 }
 
 };
--- unit_test.cpp
+++ unit_test.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
-    ::testing::GTEST_FLAG(filter) = "GlobalTest.show_mesh_test";
+    ::testing::GTEST_FLAG(filter) = "GlobalTest.draw_small_bunny";
     // ::testing::GTEST_FLAG(filter) = "GlobalTest.line_segment_intersection";
      //::testing::GTEST_FLAG(filter) = "GlobalTest.mesh_copy_faces";
      //::testing::GTEST_FLAG(filter) = "GlobalTest.edge_collapse_intersection_detect";
